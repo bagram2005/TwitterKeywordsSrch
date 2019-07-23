06c75c9c6aec8174ef95b6fcdd38cc030827ab2f
bagram2005 <la_hudson@icloud.com>
2019-07-23 07:52:36 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 // get these keys in https://apps.twitter.com/app/new 
 
 module.exports = {
-  consumer_key: 'R1V5FOuuWLBjf0Pm5dGMFyqOr',
-  consumer_secret: 'dmka4YX4B8izJvmCkWglKdoRzFJJrlPG0kUhH4r1OKoIzsAa3t',
+  consumer_key: '7R5LOy4DYPFebHK7fyTUaKiVD',
+  consumer_secret: 'HHdmjb4BnQHO4SHCkXvuLHrXXsuJUpitGhkjAVvaWtZiRGH3t7',
   access_token_key: '1145002727686230023-inbPlHNExYcpfpLqvd4ruVhytUSNJh',
   access_token_secret: 'NwHuLT48nZ8z0S0gcmNXvN3BfFWmLU5xk9sqZI19aX1m6'
 }
